a0294517724dcb026465053e46c7d47e0f1ccd70
Sattiyan Selvarajah <sattiyan@outlook.com>
2022-08-05 10:46:06 +0800

    Sign In Bootstrap Template

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php
-
-echo "Initial Commit for PHP CRUD!";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0; url=sign-in">
+    <title>Apps | Sattiyan S.</title>
+</head>
+<body>
+    
+</body>
+</html>
